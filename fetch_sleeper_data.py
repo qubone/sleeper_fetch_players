e036e049ea5ad073c259babd821f30064107ba4d
qubone <linussbo@gmail.com>
2025-01-19 22:42:29 +0100

    Added support for NBA players (#4)
    * Added support for NBA players
    
    * Automated update of Sleeper NFL data
    
    * Automated update of Sleeper NBA data
    
    * Updated README.md
    
    * Removed data directory
    
    ---------
    
    Co-authored-by: GitHub Actions <actions@github.com>

--- fetch_sleeper_data.py
+++ fetch_sleeper_data.py
@@ -3,22 +3,24 @@
 
 import json
 from pathlib import Path
-from typing import Any
+from typing import Any, Optional
 
 import requests
+import click
 
-URL = "https://api.sleeper.app/v1/players/nfl"
 
-
-def fetch_players() -> Any | None:
+def fetch_players(sport: str) -> Optional[Any]:
     """Fetches data from Sleeper API using HTTP GET.
-    Response is validated with error handling.
+
+    Args:
+        sport (str): Sport to fetch data for
 
     Returns:
-        Any | None: Response data in JSON format or None
+        Optional[Any]: Response data in JSON format or None
     """
+    url = f"https://api.sleeper.app/v1/players/{sport}"
     try:
-        response = requests.get(URL, timeout=60)
+        response = requests.get(url, timeout=60)
         response.raise_for_status()
 
         if "application/json" in response.headers.get("Content-Type", ""):
@@ -26,8 +28,6 @@ def fetch_players() -> Any | None:
         print("Error: Unexpected content type. Expected JSON.")
         return None
 
-    except requests.exceptions.HTTPError as http_err:
-        print(f"HTTP error occurred: {http_err}")
     except requests.exceptions.ConnectionError as conn_err:
         print(f"Connection error occurred: {conn_err}")
     except requests.exceptions.Timeout as timeout_err:
@@ -38,9 +38,15 @@ def fetch_players() -> Any | None:
         print(f"JSON decoding error occurred: {json_err}")
     return None
 
+@click.command()
+@click.option("--sport", default="nfl", help="Sport to fetch data for")
+def main(sport: str):
+    """Main function to fetch data and save it to a file."""
+    output_dir = Path(sport)
+    output_dir.mkdir(exist_ok=True)
+    file_path = output_dir / "sleeper_data_latest.json"
+    response_data = fetch_players(sport)
+    file_path.write_text(json.dumps(response_data, indent=4))
 
-output_dir = Path("data")
-output_dir.mkdir(exist_ok=True)
-file_name = output_dir / "sleeper_data_latest.json"
-response_data = fetch_players()
-file_name.write_text(json.dumps(response_data, indent=4))
+if __name__ == "__main__":
+    main()
